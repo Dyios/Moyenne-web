4ffc7c85d2ad16b0d11aaa88390fea85ed897dd6
Dyios <47982363+Dyios@users.noreply.github.com>
2019-07-18 17:24:00 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,174 @@
+function saisie( type , nom , champ , valeur = null)
+{   
+    let input = document.createElement("input");
+    input.setAttribute("type" , type);
+    if ( type === "number" )
+        {
+            input.setAttribute( "min" , 0 );
+            input.setAttribute( "max" , 20 );
+            input.setAttribute( "name" , nom );
+            input.setAttribute( "step" , 0.25 );
+            if ( valeur !== null )
+                input.setAttribute( "value" , valeur );
+        }
+    else{
+        input.id = nom;
+        input.readOnly = true;
+    }
+    
+    champ.appendChild( input ); 
+    return champ;
+}
+
+let table = document.getElementsByTagName("table")[0];
+
+S4.forEach( module =>{
+    let nom = document.createElement("td");
+    nom.textContent = module.nom;
+    nom.setAttribute( "width" , 400 ) ;
+    
+    let coef = document.createElement("td");
+        coef.textContent = module.coef;
+        coef.setAttribute("name" , "coeff");
+    
+    let TD = document.createElement("td");
+    if ( module.TD !== '/' )
+        {
+            saisie( "number" , module.nom , TD , module.TD );
+        }
+    else{
+        TD.textContent = module.TD ;
+    }
+    
+    let TP = document.createElement("td");
+    if ( module.TP !== '/' )
+        {
+            saisie( "number" , module.nom , TP , module.TP );
+        }
+    else
+        TP.textContent = module.TP ;
+    
+    let EX = document.createElement("td"); 
+    saisie( "number" , module.nom , EX , module.Examin);
+    
+    let moy = document.createElement("td");
+    saisie( "text" , module.nom , moy );
+    
+    let ligne = document.createElement("tr");
+    ligne.appendChild(nom);
+    ligne.appendChild(coef);
+    ligne.appendChild(TD);
+    ligne.appendChild(TP);
+    ligne.appendChild(EX);
+    ligne.appendChild(moy);
+    
+    table.appendChild(ligne);
+});
+
+let moyGeneral = document.createElement("th");
+moyGeneral.setAttribute("colspan" , 6);
+moyGeneral.textContent = "MOYENNE :";
+saisie( "text" , "moy" , moyGeneral );
+
+let ligne2 = document.createElement("tr");
+ligne2.appendChild(moyGeneral);
+table.appendChild(ligne2);
+
+////////////////////////////////////////////// TRAITEMENT ///////////////////////////////////////////////////////
+
+Number.prototype.round = function round(places) {
+    return +(Math.round(this + "e+" + places) + "e-" + places);
+}
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
+let inputs = document.querySelectorAll(" tr td input[type=number]");
+
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", function (e) {
+
+        if (e.target.value > 20)
+            e.target.value = 20;
+        if (e.target.value < 0)
+            e.target.value = 0;
+
+        let moyMod = document.getElementById(e.target.name);
+        let compt = document.getElementsByName(e.target.name);
+        moyMod.value = 0;
+        
+        if ( compt.length === 3  )
+            {
+                moyMod.value = (((Number(compt[0].value) + Number(compt[1].value)) / 2 + (Number(compt[2].value) * 2)) / 3);
+            }
+        else if ( (compt.length === 2) )
+            {
+                moyMod.value = (Number(compt[0].value) + ( Number(compt[1].value) * 2 )) / 3  ;
+            }
+        else
+            moyMod.value = compt[0].value ;
+        
+        moyMod.value = roundToTwo(moyMod.value);
+
+        let moys = document.querySelectorAll("td input[type=text]");
+        let moyg = document.querySelector("th input");
+        let coefs = document.querySelectorAll("td[name=coeff]");
+        let coefNum = 0;
+
+        moyg.value = 0;
+        for (let i = 0; i < moys.length; i++) {
+            moyg.value = Number(moyg.value) + Number(moys[i].value) * Number(coefs[i].textContent);
+            coefNum += Number(coefs[i].textContent);
+        }
+        moyg.value = Number(moyg.value) / coefNum;
+        moyg.value = roundToTwo(moyg.value);
+    });
+}
+
+window.addEventListener("load", function(){
+    let lignes = document.getElementsByTagName("tr");
+    let tabmoy = []  ;
+    let tabcoef = [] ;
+    let coefTotal = 0 ; 
+    for ( var i = 1 ; i < lignes.length-1 ; i++ )
+        {
+            let Elt = lignes[i].querySelectorAll("[name]") ;
+            
+            let moyMod = lignes[i].querySelector("[type=text]") ;
+            
+        // moyenne module
+        if ( Elt.length === 4  )
+            {
+                moyMod.value = (((Number(Elt[1].value) + Number(Elt[2].value)) / 2 + (Number(Elt[3].value) * 2)) / 3);
+            }
+        else if ( (Elt.length === 3) )
+            {
+                moyMod.value = (Number(Elt[1].value) + ( Number(Elt[2].value) * 2 )) / 3  ;
+            }
+        else
+            moyMod.value = Elt[1].value ;
+            
+        // l afficher
+            moyMod.value = roundToTwo(moyMod.value);
+            if ( moyMod.value == 0 || moyMod.value ==='NaN' )
+                {
+                    moyMod.value = "";
+                } 
+            tabmoy.push(Number(moyMod.value));
+            tabcoef.push(Number(lignes[i].querySelector("[name]").textContent));
+            coefTotal += Number(lignes[i].querySelector("[name]").textContent);
+
+        }
+            let Moyenne = document.getElementById("moy");
+            console.log(coefTotal);
+            for ( var i = 0 ; i < tabmoy.length ; i++ )
+                {
+                    Moyenne.value = Number(Moyenne.value) + Number( tabmoy[i]*tabcoef[i] ) ;
+                }
+    Moyenne.value = Number(Moyenne.value) / coefTotal ;
+    Moyenne.value = roundToTwo(Moyenne.value);
+    if (Moyenne.value==0)
+        Moyenne.value = "";
+});
---
+++ js/modules.js
@@ -0,0 +1,59 @@
+let S4 = [
+    {
+        nom : "Bases de Données (BD)" ,
+        coef : 2 ,
+        TD : null ,
+        TP : null ,
+        Examin : null 
+    } ,
+    {
+        nom : "Sysèmes d'Exploitation 1 (SE)" ,
+        coef : 3 ,
+        TD : null ,
+        TP : null ,
+        Examin : null 
+    } ,
+    {
+        nom : "Génie Logiciel 1 (GL)" ,
+        coef : 2 ,
+        TD : null ,
+        TP : '/' ,
+        Examin : null 
+    } ,
+    {
+        nom : "Théorie des graphes (THG)" ,
+        coef : 2 ,
+        TD : null ,
+        TP : '/' ,
+        Examin : null 
+    } ,
+    {
+        nom : "Réseaux de Communication (RC)" ,
+        coef : 3 ,
+        TD : null ,
+        TP : null ,
+        Examin : null 
+    } ,
+    {
+        nom : "D'éveloppement d'applications Web (DW)" ,
+        coef : 2 ,
+        TD : '/' ,
+        TP : null ,
+        Examin : null 
+    } ,
+    {
+        nom : "Aspects Juridiques et Economiques des Logiciels (AJEL)" ,
+        coef : 1 ,
+        TD : '/' ,
+        TP : '/' ,
+        Examin : null 
+    } , 
+    {
+        nom : "Anglais (ANG3)" ,
+        coef : 1 ,
+        TD : '/' ,
+        TP : '/' ,
+        Examin : null 
+    } ,
+    
+];
